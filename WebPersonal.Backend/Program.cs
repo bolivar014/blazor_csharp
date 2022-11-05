8c5e210cce0bfbd6780aeda745d412ea136569c3
Juan Camilo Valdes Bolivar <bolivar014@gmail.com>
2022-11-05 11:13:33 -0500

    configurando program.cs - agredando mapeo de endpoints

--- WebPersonal.Backend/Program.cs
+++ WebPersonal.Backend/Program.cs
@@ -3,4 +3,25 @@ var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseStaticFiles();
+
+app.UseRouting();
+
+// Mapeo de controladores nuevos
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllers();
+    endpoints.MapRazorPages();
+});
+
+app.UseAuthorization();
+
 app.Run();
